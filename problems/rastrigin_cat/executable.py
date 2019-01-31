4706be26090056cdfe6ff125e3b7c9902051005f
Prasanna <prasannaprakash@gmail.com>
2019-01-30 22:26:19 -0600

    rastrigin update

--- problems/rastrigin_cat/executable.py
+++ problems/rastrigin_cat/executable.py
@@ -20,7 +20,7 @@ def create_parser():
 
     for id in range(1,11):
         parser.add_argument('--p%d'%id, action='store', dest='p%d'%id,
-                            nargs='?', const=2, type=float, default=-5,
+                            nargs='?', const=2, type=int, default=-5,
                             help='parameter p%d value'%id)
 
     return(parser)
@@ -41,7 +41,6 @@ p9 = param_dict['p9']
 p10 = param_dict['p10']
 
 x=np.array([p1,p2,p3,p4,p5,p6,p7,p8,p9,p10])
-print(x)
 
 def rastrigin( x ):  # rast.m
     x = np.asarray_chkfinite(x)
@@ -50,6 +49,7 @@ def rastrigin( x ):  # rast.m
 
 pval = rastrigin( x )
 print('OUTPUT:%1.3f'%pval)
+
 
 
 
