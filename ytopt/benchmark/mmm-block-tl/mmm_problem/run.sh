3d765b736a185bbad52b0432d08ee8341d345159
Jaehoon Koo <jkoo@mcswl056.mcs.anl.gov>
2022-04-25 16:44:31 -0500

    edits 1

--- ytopt/benchmark/mmm-block-tl/mmm_problem/run.sh
+++ ytopt/benchmark/mmm-block-tl/mmm_problem/run.sh
@@ -1,21 +1,21 @@
 #! /usr/bin/env bash
 # source activate ytune
 export PYTHONWARNINGS="ignore"
-# cd /Users/jkoo/research/github/ytopt_tutorial/ytopt/ytopt/benchmark/mmm-block-tl/mmm_problem
-# ####
-# python -m ytopt.search.ambs --evaluator ray --problem problem_s.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge  --set-SEED 1234 
-# mv results.csv results_rf_100.csv
-# mv ytopt.log ytopt_rf_100.log
+cd /Users/jkoo/research/github/ytopt_tutorial/ytopt/ytopt/benchmark/mmm-block-tl/mmm_problem
 ####
-# python -m ytopt.search.ambs --evaluator ray --problem problem_m.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge --set-SEED 3579 
-# mv results.csv results_rf_200.csv
-# mv ytopt.log ytopt_rf_200.log
-####
-# python -m ytopt.search.ambs --evaluator ray --problem problem_l.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge 
-# mv results.csv results_rf_300.csv
-# mv ytopt.log ytopt_rf_300.log
-####
-python -m ytopt.search.ambs --evaluator ray --problem problem_target.Problem --max-evals=10 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge --set-SEED 2468 
+python -m ytopt.search.ambs --evaluator ray --problem problem_s.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge #--set-SEED 1234 
+mv results.csv results_rf_100.csv
+mv ytopt.log ytopt_rf_100.log
+###
+python -m ytopt.search.ambs --evaluator ray --problem problem_m.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge #--set-SEED 3579 
+mv results.csv results_rf_200.csv
+mv ytopt.log ytopt_rf_200.log
+###
+python -m ytopt.search.ambs --evaluator ray --problem problem_l.Problem --max-evals=30 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge 
+mv results.csv results_rf_300.csv
+mv ytopt.log ytopt_rf_300.log
+###
+python -m ytopt.search.ambs --evaluator ray --problem problem_target.Problem --max-evals=10 --learner RF --set-KAPPA 1.96 --acq-func gp_hedge #--set-SEED 2468 
 mv results.csv results_rf_500.csv
 mv ytopt.log ytopt_rf_500.log
 #### 
