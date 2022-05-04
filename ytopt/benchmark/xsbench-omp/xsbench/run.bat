e9539168179cafda5f1201884a98ab90090cde84
Xingfu Wu <38541094+wuxf99@users.noreply.github.com>
2022-05-04 17:12:04 -0500

    Update run.bat

--- ytopt/benchmark/xsbench-omp/xsbench/run.bat
+++ ytopt/benchmark/xsbench-omp/xsbench/run.bat
@@ -1,3 +1,3 @@
-python3 -m ytopt.search.ambs --evaluator ray --problem problem.Problem --max-evals=5 --learner GP
+python3 -m ytopt.search.ambs --evaluator ray --problem problem.Problem --max-evals=5 --learner RF
 python3 findMin.py
 
