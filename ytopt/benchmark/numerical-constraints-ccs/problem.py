8055b10d7791dfb2c7c3c642da664b60d29ce234
Brice Videau <bvideau@anl.gov>
2023-04-24 12:51:38 -0500

    Numerical parameter have a class of Int or Float now.

--- ytopt/benchmark/numerical-constraints-ccs/problem.py
+++ ytopt/benchmark/numerical-constraints-ccs/problem.py
@@ -11,9 +11,9 @@ cs = CCS.ConfigurationSpace(name = "numerical-constraints-ccs")
 cs.rng.seed = 1234
 
 
-p1 = CCS.NumericalParameter.int(0, 200, name='x1')
-p2 = CCS.NumericalParameter.int(0, 200, name='x2', default=1)
-p3 = CCS.NumericalParameter.int(0, 200, name='x3')
+p1 = CCS.NumericalParameter.Int(lower = 0, upper = 200, name='x1')
+p2 = CCS.NumericalParameter.Int(lower = 0, upper = 200, name='x2', default=1)
+p3 = CCS.NumericalParameter.Int(lower = 0, upper = 200, name='x3')
 
 cs.add_parameters([p1, p2, p3])
 
