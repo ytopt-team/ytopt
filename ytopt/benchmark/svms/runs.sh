f2ec1f8c56b396131fc7d2adbc4b25ab240264dd
Xingfu Wu <wuxf99@gmail.com>
2024-08-15 14:17:05 -0500

    modified:   README.md 	modified:   runs.sh

--- ytopt/benchmark/svms/runs.sh
+++ ytopt/benchmark/svms/runs.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH=~/research/tmp/ytune/ytopt-libensemble/ytopt-libe-svms/mkh/ytopt
+export PYTHONPATH=~/research/tmp/ytune/ytopt/ytopt/benchmark/svms
 python -m ytopt.search.ambs --evaluator ray --problem problem.Problem --max-evals=4 --learner RF  
 python findMin.py
 
